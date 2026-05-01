dcd2069de291fead9b258920828a67bcd52cf03a
ankit <ankitkumarswe01@gmail.com>
2026-05-01 11:06:33 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ app.use(express.json());
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE'] }));
 
 
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
     .then(() => console.log("✅ MongoDB Atlas Connected"))
     .catch(err => console.error("❌ MongoDB Error:", err));
 
